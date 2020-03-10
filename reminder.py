58919bf95b416286dc7e66eb922c9674d555b677
Boyan Kovachev <boyankovachev40@gmail.com>
2020-03-10 18:48:05 +0200

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -1,41 +1,32 @@
 import tkinter as tkr
 import time
+import sqlite3
 
 #WIDGET NAMES ARE DECLARED WITH CAPITAL LETTERS
 
 class Reminder:
     def __init__(self):
-        print('intitalizaing reminder object')
+        #konstruktora setupva neshtata
         self.reminder = tkr.Tk()
-        self.transparency = 0.5
-        self.message = 'nik e gei'
-        self.time_between_reminders = 2
-        self.reminder_time_on_screen = 2
-        
-        self.set_up_message()
-
+        self.read_from_database()
+        self.set_up_message_widget()
         self.reminder.wm_attributes("-alpha",self.transparency)
         self.reminder.overrideredirect(1)
         self.center()
         
 
+    #not used useless
     def run(self):
-        print('running mainloop reminder')
         self.reminder.mainloop()
-        #useless ne moe go barame blokira vsichkiq kod dolu
-        #do spirane na tkinera
 
+    #not used useless
     def quit(self):
-        print('closing mainloop reminder')
         self.reminder.quit()
-        #useless
         
     def update(self):
-        print('updating reminder')
         self.reminder.update()
 
     def destroy(self):
-        print('destroying reminder')
         self.reminder.destroy()
 
 
@@ -69,11 +60,39 @@ class Reminder:
         self.reminder.geometry('{}x{}+{}+{}'.format(width, height, x, y))
         self.reminder.deiconify()
 
-    def set_up_message(self):
+    def set_up_message_widget(self):
         self.MESSAGE = tkr.Message(self.reminder, text = self.message)
         self.MESSAGE.config(background = 'white')
         self.MESSAGE.config(font = ('times', 100, 'italic'))
         self.MESSAGE.pack()
+
+    def read_from_database(self):
+        conn = sqlite3.connect('database.db')
+        cur = conn.cursor()
+
+        cur.execute("SELECT message FROM settings")
+        temp = cur.fetchall()
+        self.message = temp[0][0]
+
+        cur.execute("SELECT transparency FROM settings")
+        temp = cur.fetchall()
+        self.transparency = temp[0][0]
+
+        cur.execute("SELECT time_between_reminders FROM settings")
+        temp = cur.fetchall()
+        self.time_between_reminders = temp[0][0]
+
+        cur.execute("SELECT time_of_reminder_on_screen FROM settings")
+        temp = cur.fetchall()
+        self.reminder_time_on_screen = temp[0][0]
+
+        cur.execute("SELECT process_status FROM settings")
+        temp = cur.fetchall()
+        if(temp[0][0] == 1):
+            self.process_status = True
+        else:
+            self.process_status = False
+        
 
 while(True):
     reminder = Reminder()
